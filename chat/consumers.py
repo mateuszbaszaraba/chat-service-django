7c47607da05be4fbb0a280ae7acb2273317945e2
Mateusz Baszaraba <mateuszbaszaraba@outlook.com>
2022-02-10 09:34:37 +0100

    Added user before sent message

--- chat/consumers.py
+++ chat/consumers.py
@@ -14,21 +14,6 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'tester_message',
-                'tester': 'testing message'
-            }
-        )
-
-    async def tester_message(self, event):
-        tester = event['tester']
-
-        await self.send(text_data=json.dumps({
-            'tester': tester
-        }))
-
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
@@ -38,20 +23,24 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        username = text_data_json['username']
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chatroom_message',
-                'message': message
+                'message': message,
+                'username': username
             }
         )
 
     async def chatroom_message(self, event):
         message = event['message']
+        username = event['username']
 
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'username': username
         }))
 
     pass
